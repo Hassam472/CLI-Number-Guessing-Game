3bd899367619cb64ecefbe466dca3eedb9636a59
M Hassam <159873960+Hassam472@users.noreply.github.com>
2024-07-05 02:04:05 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumbers = Math.floor(Math.random() * 6 + 1);
+const result = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1-6: ",
+    }
+]);
+if (result.userGuessedNumber === randomNumbers) {
+    console.log("Congratulations, You Won!");
+}
+else {
+    console.log("Game Over");
+}
+;
